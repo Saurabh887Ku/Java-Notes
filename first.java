21ad2c264d431d757d7096bb95229026b94d53ea
Saurabh Kumar <sk88714380@gmail.com>
2024-08-15 13:16:41 +0530

    First Commit

---
+++ first.java
@@ -0,0 +1,8 @@
+
+public class first{
+    public static void main(String[] Saurabh){
+        // Data type is written before which type of value is going to store.
+        int age = 23;
+        System.out.println(age);
+    }
+}
